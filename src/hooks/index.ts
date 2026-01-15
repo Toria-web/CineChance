7028425a6821c2f3a8040079578c49812261f9ef
Alexander <68967378+AlexanderNord@users.noreply.github.com>
2026-01-15 13:10:20 +0000

    v.1.1.9-7-8-3.4-2.1-Nord

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,3 @@
 // src/hooks/index.ts
 export { useSearch } from './useSearch';
-export { useBatchData, useInvalidateBatchData } from './useBatchData';
+export { useBatchData } from './useBatchData';
